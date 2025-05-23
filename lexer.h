d03b3562c5c702090246def9bdeae7d7c4047897
Victor <victor.gabrielnord@gmail.com>
2025-05-23 16:55:26 -0300

    test

---
+++ lexer.h
@@ -0,0 +1,34 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+//Lista de TOKENS
+typedef enum{
+    TOKEN_ENUM, // número inteiro (ex.: "123")
+    TOKEN_IDEN, // identificador (ex.: "x", "foo")
+    TOKEN_PRINT, // palavra‐reservada "print"
+    TOKEN_PLUS, // '+'
+    TOKEN_MINUS, // '-'
+    TOKEN_STAR, // '*'
+    TOKEN_SLASH, // '/'
+    TOKEN_LPAREN, // '('
+    TOKEN_RPAREN,  // ')'
+    TOKEN_SEMICOLON, // ';'
+    TOKEN_ASSIGN, // '='
+    TOKEN_EOF, // fim do arquivo/input
+    TOKEN_INVALID // qualquer caractere que não pertença a nada acima
+} TokenType;
+
+//Estrutura do TOKEN
+typedef struct {
+    TokenType type;
+    char lexeme[64]; //copiar o texto
+    int value;
+} Token;
+
+//Função para iniciar o LEXER com uma estrutura string
+void init_lexer(const char *input);
+
+//Retorna o TOKEN seguinte
+Token next_token(void);
+
+#endif 
