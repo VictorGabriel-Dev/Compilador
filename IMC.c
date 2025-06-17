0b0ff5d8188571cdd8e46ad51f222ec7d4182ff5
JacquesXXL <135280900+JacquesXXL@users.noreply.github.com>
2025-06-17 12:21:31 -0300

    Update IMC.c

--- IMC.c
+++ IMC.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
+int main() {
+    float peso, altura, imc;
+    int UAltura;
 
-int main () {
+    printf("Digite seu peso (kg): ");
+    scanf("%f", &peso);
 
-float massa, altura, imc;
+    printf("\nSua altura está em:\n");
+    printf("1 - Metros \n");
+    printf("2 - Centímetros \n");
+    printf("Escolha: ");
+    scanf("%d", &UAltura);
 
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
 
-printf("Digite sua massa em kg: ");
-scanf("%f", &massa);
-printf("Digite sua altura em metros: ");
-scanf("%f", &altura);
+    if (UAltura == 2) {
+        altura = altura / 100;
+    }
 
-imc = massa / (altura * altura);
+    imc = peso / (altura * altura);
 
-if(imc < 18.5) {
-    printf("Abaixo do peso\n");
-} else if(imc >= 18.5 && imc < 24.9) {
-    printf("Peso normal\n");
-} else if(imc >= 25 && imc < 29.9) {
-    printf("Sobrepeso\n");
-} else {
-    printf("Obesidade\n");
-}
-printf("Seu IMC é: %.2f", imc);
+    printf("\nSeu IMC é: %.2f\n", imc);
+    printf("Classificação: ");
+
+    if (imc <= 18.5) {
+        printf("Abaixo do Peso\n");
+    } else if (imc > 18.5 && imc <= 24.9) {
+        printf("Saudável\n");
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        printf("Peso em Excesso\n");
+    } else if (imc >= 30.0 && imc <= 34.9) {
+        printf("Obesidade Grau I\n");
+    } else if (imc >= 35.0 && imc <= 39.9) {
+        printf("Obesidade Grau II (Severa)\n");
+    } else {
+        printf("Obesidade Grau III (Mórbida)\n");
+    }
 
     return 0;
+}
